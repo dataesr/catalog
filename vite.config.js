f624e2e9caf0c385aea11f3b7871685c9d8ec48c
Anne L'Hôte <anne.lhote@gmail.com>
2023-08-07 13:20:31 +0200

    fix(ci): Should deploy now /3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.BASE,
+  build: {
+    emptyOutDir: true,
+    outDir: '../dist'
+  },
   plugins: [react()],
 })
