16cf504dc04547563010fe1d5d7d63366eb9afab
Anne L'Hôte <anne.lhote@gmail.com>
2023-08-07 13:44:17 +0200

    fix(ci): Clean

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/catalog/',
-  build: {
-    emptyOutDir: true,
-    outDir: './dist'
-  },
   plugins: [react()],
 })
