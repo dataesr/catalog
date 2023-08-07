464b83390e9fe8b702b7f1101bfd44c529bde2d2
Anne L'Hôte <anne.lhote@gmail.com>
2023-08-07 14:21:22 +0200

    fix(ci): Clean deployment and config base url /2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-const VITE_BASE = process.env.VITE_BASE || '/';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: VITE_BASE,
   plugins: [react()],
 })
