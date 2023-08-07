f129bfdef0fdb50a7f7cbef69970eb7d62808360
Anne L'Hôte <anne.lhote@gmail.com>
2023-08-07 13:38:30 +0200

    fix(ci): Should deploy now for real /6

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { HashRouter } from 'react-router-dom';
 
 import Router from './router';
 import './styles/index.scss';
@@ -11,11 +11,11 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools />
         <Router />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
